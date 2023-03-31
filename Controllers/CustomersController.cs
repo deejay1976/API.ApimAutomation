9948405ca37fb5d167305731d520d4faa8de016b
Dhananjay Kumar <dkumar.sa@hotmail.com>
2023-03-31 11:09:20 +0100

    Swagger content

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using MHR.API.ApimAutomation.Models;
+using MHR.API.ApimAutomation;
 
 namespace MHR.API.ApimAutomation.Controllers
 {
@@ -32,6 +32,11 @@ namespace MHR.API.ApimAutomation.Controllers
         }
 
         // GET: api/Customers/5
+        /// <summary>
+        /// Get the Customer Details based upon the customer id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("{id}")]
         public async Task<ActionResult<TblCustomer>> GetTblCustomer(int id)
         {
@@ -51,6 +56,12 @@ namespace MHR.API.ApimAutomation.Controllers
 
         // PUT: api/Customers/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        /// <summary>
+        /// Update the customer details 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="tblCustomer"></param>
+        /// <returns></returns>
         [HttpPut("{id}")]
         public async Task<IActionResult> PutTblCustomer(int id, TblCustomer tblCustomer)
         {
@@ -82,6 +93,11 @@ namespace MHR.API.ApimAutomation.Controllers
 
         // POST: api/Customers
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        /// <summary>
+        /// Add a customer
+        /// </summary>
+        /// <param name="tblCustomer"></param>
+        /// <returns></returns>
         [HttpPost]
         public async Task<ActionResult<TblCustomer>> PostTblCustomer(TblCustomer tblCustomer)
         {
@@ -110,6 +126,11 @@ namespace MHR.API.ApimAutomation.Controllers
         }
 
         // DELETE: api/Customers/5
+        /// <summary>
+        /// Delete the customer details
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteTblCustomer(int id)
         {
@@ -128,7 +149,11 @@ namespace MHR.API.ApimAutomation.Controllers
 
             return NoContent();
         }
-
+        /// <summary>
+        /// Find out if a customer exists in record
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         private bool TblCustomerExists(int id)
         {
             return (_context.TblCustomers?.Any(e => e.Id == id)).GetValueOrDefault();
--- Program.cs
+++ Program.cs
@@ -1,16 +1,36 @@
+using MHR.API.ApimAutomation;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.OpenApi.Models;
 using System.Configuration;
+using System.Reflection;
+using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 //builder.Services.AddDbContext<>
 var constr = builder.Configuration.GetValue<string>("ConnectionStrings:CustomerContext","");
 builder.Services.AddControllers();
-//builder.Services.AddDbContext<CustomerDbContext>(options => options.UseSqlServer(constr));
+builder.Services.AddDbContext<CustomerDbContext>(options => options.UseSqlServer(constr));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen
+    
+    (
+        c =>
+        {
+            // Set Title and version from config
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Customer API", Version = "1.0", Description = "Use these APIs to get the Customer, their contact and address. " });
+            // Set the comments path for the Swagger JSON and UI.
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            // pick comments from classes, include controller comments: another tip from StackOverflow
+            c.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
+            // enable the annotations on Controller classes [SwaggerTag]
+            c.EnableAnnotations();
+            // to allow for a header parameter
+            c.OperationFilter<SwaggerExtensionFilter>();
+        });
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
